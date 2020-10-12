9b8cbd0915463b9062c77510e80f6a8ebea23138
arushanovanz <nadin.spb@gmail.com>
2020-10-12 12:13:48 +0300

    Accept.content type requestSpec builder

--- src/test/java/restassuredTests/RestAssuredTests.java
+++ src/test/java/restassuredTests/RestAssuredTests.java
@@ -1,15 +1,16 @@
 package restassuredTests;
 
 import io.restassured.RestAssured;
-import io.restassured.authentication.OAuthSignature;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
+import static io.restassured.config.XmlConfig.xmlConfig;
 
 public class RestAssuredTests {
-
-
     @Test
     public void getEndpointTest(){
 
@@ -21,7 +22,27 @@ public class RestAssuredTests {
                     .oauth2("")
                .with()
                .when().get("/info")
-               .then().statusCode(200);
+               .then().log().all().statusCode(200);
+
+    }
+    @Test
+    public void getEndpointHeaderTest(){
+        String key = "91862a20f9msh633e027463600f2p1ca3edjsn9e88f1e2dd14";
+
+       RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setBaseUri("https://omgvamp-hearthstone-v1.p.rapidapi.com/")
+                .setAccept(ContentType.JSON)
+                .addHeader("x-rapidapi-key",key)
+                .log(LogDetail.ALL)
+                .build();
+
+       given()
+
+//                .spec(rs)
+                .with()
+                .when().get("/info")
+                .then()
+                .log().all().statusCode(200);
 
     }
 }
